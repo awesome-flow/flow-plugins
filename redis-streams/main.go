d5dbeec0e7e2b5197c0b36acc255677fb050df52
Oleg Sidorov <me@whitebox.io>
2019-01-29 00:21:50 +0100

    Imported redis library and implemented some more setup logic

--- redis-streams/main.go
+++ redis-streams/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/go-redis/redis"
+
 	"github.com/awesome-flow/flow/pkg/core"
 	"github.com/awesome-flow/flow/pkg/devenv"
 )
@@ -8,13 +10,19 @@ import (
 func main() {}
 
 type RedisStreams struct {
+	addr   string
+	passwd string
+	db     int
 	*core.Connector
 }
 
 var _ core.Link = &RedisStreams{}
 
 func New(name string, params core.Params, context *core.Context) (core.Link, error) {
-	rsl := &RedisStreams{}
+	rsl := &RedisStreams{
+		"", "", 0,
+		core.NewConnectorWithContext(context),
+	}
 	//TODO(olegs)
 
 	rsl.OnSetUp(rsl.SetUp)
@@ -24,6 +32,20 @@ func New(name string, params core.Params, context *core.Context) (core.Link, err
 }
 
 func (rsl *RedisStreams) SetUp() error {
+	ctx := rsl.Connector.GetContext()
+	var client *redis.Client
+	client_i, ok := ctx.GetVal("redis-client")
+	if ok {
+		client = client_i.(*redis.Client)
+	} else {
+		client = redis.NewClient(&redis.Options{
+			Addr:     rsl.addr,
+			Password: rsl.passwd,
+			DB:       rsl.db,
+		})
+		ctx.SetVal("redis-client", client)
+	}
+
 	//TODO(olegs)
 	return nil
 }
