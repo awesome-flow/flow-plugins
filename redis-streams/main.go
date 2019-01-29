0866589aae841df82d900365e92530aa21fda50d
Oleg Sidorov <me@whitebox.io>
2019-01-29 22:20:55 +0100

    Completed Redis-streams plugin implementation

--- redis-streams/main.go
+++ redis-streams/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+
+	"github.com/awesome-flow/flow/pkg/metrics"
+
 	"github.com/go-redis/redis"
 
 	"github.com/awesome-flow/flow/pkg/core"
@@ -9,21 +14,45 @@ import (
 
 func main() {}
 
+const MetricsBasePath = "plugins.sink.redis-streams"
+
 type RedisStreams struct {
-	addr   string
-	passwd string
-	db     int
+	address  string
+	password string
+	db       int
+	client   *redis.Client
 	*core.Connector
 }
 
 var _ core.Link = &RedisStreams{}
 
 func New(name string, params core.Params, context *core.Context) (core.Link, error) {
-	rs := &RedisStreams{
-		"", "", 0,
-		core.NewConnectorWithContext(context),
+
+	var address string
+	if v, ok := params["address"]; ok {
+		address = v.(string)
+	} else {
+		return nil, fmt.Errorf("redis-streams is missing a mandatory argument addres")
 	}
-	//TODO(olegs)
+
+	var password string
+	if v, ok := params["password"]; ok {
+		password = v.(string)
+	} else {
+		password = ""
+	}
+
+	var db int
+	if v, ok := params["db"]; ok {
+		db = v.(int)
+	} else {
+		db = 0
+	}
+
+	rs := &RedisStreams{
+		address, password, db,
+		nil, core.NewConnectorWithContext(context),
+	}
 
 	rs.OnSetUp(rs.SetUp)
 	rs.OnTearDown(rs.TearDown)
@@ -34,25 +63,83 @@ func New(name string, params core.Params, context *core.Context) (core.Link, err
 func (rs *RedisStreams) SetUp() error {
 	ctx := rs.Connector.GetContext()
 	var client *redis.Client
-	client_i, ok := ctx.GetVal("redis-client")
+	iclient, ok := ctx.GetVal("redis-client")
 	if ok {
-		client = client_i.(*redis.Client)
+		client = iclient.(*redis.Client)
 	} else {
 		client = redis.NewClient(&redis.Options{
-			Addr:     rs.addr,
-			Password: rs.passwd,
+			Addr:     rs.address,
+			Password: rs.password,
 			DB:       rs.db,
 		})
 		ctx.SetVal("redis-client", client)
 	}
+	rs.client = client
 
-	//TODO(olegs)
 	return nil
 }
 
 func (rs *RedisStreams) TearDown() error {
-	//TODO(olegs)
+	if rs.client != nil {
+		return rs.client.Close()
+	}
 	return nil
+}
+
+func (rs *RedisStreams) Recv(msg *core.Message) error {
+	metrics.GetCounter(MetricsBasePath + ".received").Inc(1)
+	if rs.client == nil {
+		metrics.GetCounter(MetricsBasePath + ".conn_failed").Inc(1)
+		return msg.AckFailed()
+	}
+	xargs, err := ParseXAddMsg(msg)
+	if err != nil {
+		metrics.GetCounter(MetricsBasePath + ".bad_format").Inc(1)
+		return msg.AckFailed()
+	}
+	if err := rs.client.XAdd(xargs).Err(); err != nil {
+		metrics.GetCounter(MetricsBasePath + ".send_failed").Inc(1)
+		return msg.AckFailed()
+	}
+	metrics.GetCounter(MetricsBasePath + ".sent").Inc(1)
+	return msg.AckDone()
+}
+
+var splitter = []byte(" ")
+
+// ParseXAddMsg takes a pointer to a message and parses it into
+// Redis XAdd arguments according to the format:
+//   mystream * sensor-id 1234 temperature 19.8
+// The mesage payload should have at least 2 words: the stream
+// name and the ID (optional, * if empty).
+// Keys and values are converted into string tuples.
+// Keys are expected to be unique, last key wins.
+func ParseXAddMsg(msg *core.Message) (*redis.XAddArgs, error) {
+	chunks := bytes.Split(msg.Payload(), splitter)
+	if len(chunks) < 2 || (len(chunks)%2 > 0) {
+		return nil, fmt.Errorf("Malformed XAdd message: %q", msg.Payload())
+	}
+	xargs := &redis.XAddArgs{
+		Stream: string(chunks[0]),
+		Values: make(map[string]interface{}),
+	}
+
+	if string(chunks[1]) != "*" {
+		xargs.ID = string(chunks[1])
+	}
+
+	chunks = chunks[2:]
+	for len(chunks) > 0 {
+		key, value := string(chunks[0]), string(chunks[1])
+		xargs.Values[key] = value
+		chunks = chunks[2:]
+	}
+
+	return xargs, nil
+}
+
+func (rs *RedisStreams) ConnectTo(core.Link) error {
+	panic("Redis-streams sink is not supposed to be connnected")
 }
 
 func (rs *RedisStreams) DevEnv(context *devenv.Context) ([]devenv.Fragment, error) {
